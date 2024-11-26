a0ba4f0cc43cab179f93ab1a6cce59b449b3cc82
Gustavo Oliveira <gusta.olv036@gmail.com>
2024-11-25 21:41:24 -0300

    Update GerencArquivos.java

--- GerencArquivos.java
+++ GerencArquivos.java
@@ -1,5 +1,27 @@
 package Gerenciador;
 
-public class GerencArquivos {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class GerencArquivos {
+    public static void salvarDados(String arquivo, List<String> dados) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+            for (String dado : dados) {
+                writer.write(dado);
+                writer.newLine();
+            }
+        }
+    }
+
+    public static List<String> carregarDados(String arquivo) throws IOException {
+        List<String> dados = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                dados.add(linha);
+            }
+        }
+        return dados;
+    }
 }
